983db91dbd113a8eee9992a9f2d48969bbd744c7
vuurball <oneindigedroom@gmail.com>
2020-06-21 21:05:27 +0300

    completed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ai-2-knight-or-knave
-AI solving Knight-or-Knave puzzles using propositional logic
+### AI solves logic puzzles using 'propositional logic'
+
+In a Knights and Knaves puzzle, the following information is given: Each character is either a knight or a knave. A knight will always tell the truth: if knight states a sentence, then that sentence is true. Conversely, a knave will always lie: if a knave states a sentence, then that sentence is false.
+
+The objective of the puzzle is, given a set of sentences spoken by each of the characters, determine, for each character, whether that character is a knight or a knave.
+
+![](https://github.com/vuurball/ai-2-knight-or-knave/blob/master/demo.gif)
--- puzzle.py
+++ puzzle.py
@@ -9,35 +9,76 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
-# Puzzle 0
-# A says "I am both a knight and a knave."
+rules = And(
+    Biconditional(AKnight, Not(AKnave)),
+    Biconditional(BKnight, Not(BKnave)),
+    Biconditional(CKnight, Not(CKnave)),
+)
+
+print('Puzzle 0:')
+print('    > A says "I am both a knight and a knave."')
+sent1 = And(AKnight,AKnave)
 knowledge0 = And(
-    # TODO
+    rules,
+    Biconditional(AKnight, sent1)
 )
 
-# Puzzle 1
-# A says "We are both knaves."
-# B says nothing.
+print('Puzzle 1:')
+print('    > A says "We are both knaves."')
+print('    > B says nothing.')
+sent1 = And(AKnave, BKnave)
+
 knowledge1 = And(
-    # TODO
+    rules, 
+    Biconditional(AKnight, sent1)
 )
 
-# Puzzle 2
-# A says "We are the same kind."
-# B says "We are of different kinds."
+print('Puzzle 2:')
+print('    > A says "We are the same kind."')
+print('    > B says "We are of different kinds."')
+sent1 = Or(And(AKnave, BKnave), And(AKnight, BKnight))
+sent2 = Or(And(AKnight, BKnave), And(AKnave, BKnight))
+
 knowledge2 = And(
-    # TODO
+    rules,
+    Biconditional(AKnight, sent1),
+    Biconditional(BKnight, sent2)
 )
 
-# Puzzle 3
-# A says either "I am a knight." or "I am a knave.", but you don't know which.
-# B says "A said 'I am a knave'."
-# B says "C is a knave."
-# C says "A is a knight."
+print('Puzzle 3:')
+print('    > A says either "I am a knight." or "I am a knave.", but you dont know which.')
+print('    > B says "A said `I am a knave`.')
+print('    > B says "C is a knave.')
+print('    > C says "A is a knight.')
+
+sent2 = And(AKnight, AKnave)
+sent3 = CKnave
+sent4 = AKnight
+
 knowledge3 = And(
-    # TODO
+    rules,
+    Biconditional(BKnight, sent2),
+    Biconditional(BKnight, sent3),
+    Biconditional(CKnight, sent4),
 )
 
+print('Puzzle 4:')
+print('    > A says "B and I are the same.')
+print('    > B says "C is a knave')
+print('    > C says "A and C are different.')
+
+sent1 = Or(And(BKnight, AKnight), And(BKnave, AKnave))
+sent2 = CKnave
+sent3 = Or(And(AKnight, CKnave), And(AKnave, CKnight))
+
+knowledge4 = And(
+    rules,
+    Biconditional(AKnight, sent1),
+    Biconditional(BKnight, sent2),
+    Biconditional(CKnight, sent3),
+)
+
+print("")
 
 def main():
     symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
@@ -45,8 +86,10 @@ def main():
         ("Puzzle 0", knowledge0),
         ("Puzzle 1", knowledge1),
         ("Puzzle 2", knowledge2),
-        ("Puzzle 3", knowledge3)
+        ("Puzzle 3", knowledge3),
+        ("Puzzle 4", knowledge4)
     ]
+    print("SOLUTIONS:")
     for puzzle, knowledge in puzzles:
         print(puzzle)
         if len(knowledge.conjuncts) == 0:
